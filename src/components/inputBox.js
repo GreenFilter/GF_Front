6a30b67ce6da67b9f72a72882510a7b2d528fad2
hyein0112 <bhi12134@gmail.com>
2022-09-27 21:38:03 +0900

    update

---
+++ src/components/inputBox.js
@@ -0,0 +1,46 @@
+/** @jsxImportSource @emotion/react */
+import { jsx, css } from '@emotion/react'
+import React from "react";
+
+function InputBox({margin, label, placeholder}) {
+  return (
+    <div css={css`
+      font-family: 'Inter', BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+      font-size: 1.56rem;
+      `}>
+        <span css={
+          css`
+            margin-right: ${margin}px;
+            text-align: left;
+          `
+        }>
+          {label}
+        </span>
+        <input css={
+          css`
+            width: 609px;
+            height: 75px;
+            background: #FBFBFB;
+            border: 0;
+            box-shadow: 1px 2px 3px 1px rgba(0, 0, 0, 0.25);
+            border-radius: 20px;
+            font-family: 'Inter', BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+            font-size: 1.56rem;
+            padding: 0 0 0 50px;
+
+            &::placeholder{
+              color: #9B9B9B;
+            }
+            &:focus{
+              outline: none;
+            }
+            
+          `}
+          placeholder={`${placeholder}를 입력하세요.`} 
+        />
+    </div>
+  )
+  
+}
+
+export default InputBox;
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,19 @@
+/** @jsxImportSource @emotion/react */
+import { jsx, css } from '@emotion/react'
 import React from "react";
-import LoginBG from "../components/loginBackgound";
+import * as I from "../assets/svg";
+import InputBox from '../components/inputBox';
 
 function Login() {
   return (
-    <div>
-      <LoginBG />
+    <div css={css`
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    `}>
+      <I.Logo />
+      <InputBox margin="49" label="ID" placeholder="아이디" />
+      <InputBox margin="34" label="PW" placeholder="비밀번호" />
     </div>
   );
 }
