a8a56410b374b97e0fdbae2f0db8d99c658b0c7a
hyein0112 <bhi12134@gmail.com>
2022-09-30 13:59:05 +0900

    forgetButton

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,6 +3,7 @@ import { jsx, css } from '@emotion/react'
 import React from "react";
 import * as I from "../assets/svg";
 import InputBox from '../components/inputBox';
+import ForgetButton from '../components/forgetButton';
 
 function Login() {
   return (
@@ -14,6 +15,7 @@ function Login() {
       <I.Logo />
       <InputBox margin="49" inputMargin="23" label="ID" placeholder="아이디" />
       <InputBox margin="34" inputMargin="15" label="PW" placeholder="비밀번호" />
+      <ForgetButton />
     </div>
   );
 }
