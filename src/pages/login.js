8e1c4e9cf53adcfaa9e407d2920ddae96b04036b
hyein0112 <bhi12134@gmail.com>
2022-10-03 21:45:21 +0900

    background image

--- src/pages/login.js
+++ src/pages/login.js
@@ -9,15 +9,33 @@ import LoginButton from '../components/loginButton';
 function Login() {
   return (
     <div css={css`
+      height: 100vh;
+      margin: 0;
       display: flex;
       flex-direction: column;
       align-items: center;
-      margin-top: 250px;
+      background-size: 100vw 100vh;
+      background-repeat: no-repeat;
+      background-image: url('/image/backgroundLine.svg');
     `}>
+      
+      <div css={css`margin-top: 250px;`}>
       <I.Logo />
+      </div>
       <InputBox margin="49" inputMargin="33px 0 23px 0" label="ID" placeholder="아이디" />
       <InputBox margin="34" inputMargin="0 0 15px 0" label="PW" placeholder="비밀번호" />
+      <div css={
+        css`
+          width: 559px; 
+          margin-left: 50px;
+
+          @media screen and (max-width: 800px){
+          width: 65vw;
+          margin-left: 50px;
+          }
+        `}>
       <ForgetButton />
+      </div>
       <LoginButton />
     </div>
   );
