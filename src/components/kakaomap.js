6d2d2859e67dfe90adeaf9c030db95a039846c01
hyein0112 <bhi12134@gmail.com>
2022-10-12 14:50:56 +0900

    현재 위도경도

--- src/components/kakaomap.js
+++ src/components/kakaomap.js
@@ -7,22 +7,37 @@ const { kakao } = window;
 
 const Kakaomap = () => {
   const [map,setMap] = useState(null);
+  const [lat, setLat] = useState(null);
+  const [lon, setLon] = useState(null);
 
+  function onGeoOk(position) {
+    setLat(position.coords.latitude);  // 위도
+    setLon(position.coords.longitude);  // 경도
+  };
+  
+  function onGeoError() {
+    alert("Can't find you");
+  }
+
+  navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
   useEffect(()=>{
-      const container = document.getElementById('map');
-      const options = { center: new kakao.maps.LatLng(35.02183802890798, 126.78367150900264) };
-      const kakaoMap = new kakao.maps.Map(container, options);
-
-      const markerPosition  = new kakao.maps.LatLng(35.02183802890798, 126.78367150900264); 
-      const marker = new kakao.maps.Marker({
-        position: markerPosition
+    console.log(lat, lon)
+    const container = document.getElementById('map');
+    const options = { center: new kakao.maps.LatLng(lat, lon) };
+    const kakaoMap = new kakao.maps.Map(container, options);
+    
+    const markerPosition  = new kakao.maps.LatLng(lat, lon); 
+    const marker = new kakao.maps.Marker({
+      position: markerPosition
     });
     marker.setMap(kakaoMap);
-      setMap(kakaoMap);
-  },[])
+    setMap(kakaoMap);
+
+  },[lat, lon])
+
 
   return (
-      <div id="map" css={css`width: 80%; height: 735px; margin: 0 auto; border-radius: 20px;`}></div>
+      <div id="map" css={css`width: 89.17%; height: 735px; margin: 0 auto; border-radius: 20px; box-shadow: 2px 4px 5px rgba(0, 0, 0, 0.25);`}></div>
   );
 };
 
